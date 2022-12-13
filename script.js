c1f6472b42d5b24501ccc618a3d9d0d389bdcc78
DR3DD <64263028+InternetLmao@users.noreply.github.com>
2022-12-13 16:56:54 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const signInBtn = document.querySelector('.signin-btn');
+const signUpBtn = document.querySelector('.signup-btn');
+const formBox = document.querySelector('.form-box');
+
+signUpBtn.addEventListener('click', function () {
+  formBox.classList.add('active');
+  footer.classList.add('active');
+});
+
+signInBtn.addEventListener('click', function () {
+  formBox.classList.remove('active');
+  footer.classList.remove('active');
+});
